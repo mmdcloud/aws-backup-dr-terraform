9b3bccd31b40e3205446ae6ce03f547502a0b749
mo1911997 <mmd26.aka@gmail.com>
2024-07-30 13:42:52 +0530

    update: changed user_data bash script

--- scripts/user_data.sh
+++ scripts/user_data.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install -y nginx
-
 sudo apt update
 curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt install nodejs -y
+cd /home/ubuntu
+git clone https://github.com/mmdcloud/aws-autoscaling-with-load-balancing
+cd aws-autoscaling-with-load-balancing
+sudo cp scripts/nodejs_nginx.config /etc/nginx/sites-available/default
+sudo service nginx restart
+sudo npm i
+sudo npm i -g pm2
+pm2 start server.mjs
